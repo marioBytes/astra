b7d00f52b4acfee0b1a5e34f6bf741379972833d
marioBytes <mario.bytes92@gmail.com>
2024-07-09 16:01:28 -0600

    redirect signed in users to their trips

--- lib/astra_web/controllers/page_controller.ex
+++ lib/astra_web/controllers/page_controller.ex
@@ -4,6 +4,12 @@ defmodule AstraWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    if conn.assigns.current_user do
+      conn
+      |> redirect(to: "/trips")
+      |> halt()
+    else
+      render(conn, :home, layout: false)
+    end
   end
 end
